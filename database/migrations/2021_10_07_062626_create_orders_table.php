e42bf53090b8013f0e4218573f2a04699017b9c8
Alex <oleksii.spesyvtsev@computools.com>
2021-10-07 09:32:34 +0300

    create migrations

---
+++ database/migrations/2021_10_07_062626_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id',true)->index()->unique();
+            $table->unsignedBigInteger('customer_id')->index()->nullable();
+            $table->unsignedBigInteger('order_product_id')->index()->nullable();
+            $table->time('time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
